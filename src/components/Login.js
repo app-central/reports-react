40883fd509ef45bfe1d10584912877dc3c1c23a9
Chenky100 <chenky100@gmail.com>
2020-10-28 01:06:14 +0200

    style

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,13 +4,13 @@ export default function Login(props) {
 
     if (!props.start) {
         return (
-            <div className="form-box">
+            <form onSubmit={() => { props.login() }} className="form-box">
                 <label for="exampleInputPassword1">Password</label>
 
                 <input class="form-control" type="password" onChange={(e) => props.handleSetPass(e.target.value)} />
                 <br />
-                <button class="btn btn-primary mb-2" onClick={() => { props.login() }}> show reports</button>
-            </div>
+                <button class="btn btn-primary mb-2" > show reports</button>
+            </form>
         )
     } else {
         return (
